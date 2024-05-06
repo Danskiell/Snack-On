6e0dd0d835112436d40f245d9f940c0f07667954
Daniel Alves de Moura <142283468+Danskiell@users.noreply.github.com>
2024-05-06 15:23:44 -0300

    Alterações finais de estrutura de cadastro

--- app/src/main/java/com/example/snack/LoginActivity.java
+++ app/src/main/java/com/example/snack/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.snack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.example.snack.R;
 
@@ -12,5 +15,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    public void register(View view){
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
     }
 }
