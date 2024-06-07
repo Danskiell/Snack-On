ead8b402076463cc098b4447058d3cf44dc2a175
Daniel Alves de Moura <142283468+Danskiell@users.noreply.github.com>
2024-06-07 15:45:48 -0300

    Telas de esqueceu senha concluida

--- app/src/main/java/com/example/snack/SenhaNovaActivity.java
+++ app/src/main/java/com/example/snack/SenhaNovaActivity.java
@@ -10,28 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.snack.R;
-
-public class SenhaCodeActivity extends AppCompatActivity {
+public class SenhaNovaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_senha_code);
+        setContentView(R.layout.activity_senha_nova);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void voltar(View view) {
-        startActivity(new Intent(SenhaCodeActivity.this, LoginActivity.class));
-
+    public void validar(View view) {
+        startActivity(new Intent(SenhaNovaActivity.this, LoginActivity.class));
     }
 
-    public void mudarsenha(View view) {
-        startActivity(new Intent(SenhaCodeActivity.this, SenhaNovaActivity.class));
-    }
 
 }
